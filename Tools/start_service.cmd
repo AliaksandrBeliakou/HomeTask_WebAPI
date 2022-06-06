0aa470fc71b8f52c5dbff190e49d79b8555c122e
Aliaksandr Beliakou <Aliaksandr_Beliakou@epam.com>
2022-06-06 18:59:27 +0300

    Additional files

--- Tools/start_service.cmd
+++ Tools/start_service.cmd
@@ -1,1 +1,1 @@
-dotnet run --project .\WebAPI.HomeTask.NorthwindService.csproj --urls "http://localhost:10000;https://localhost:10100"
+dotnet run --project ..\WebAPI.HomeTask.NorthwindService\WebAPI.HomeTask.NorthwindService.csproj --urls "http://localhost:10000;https://localhost:10100"
